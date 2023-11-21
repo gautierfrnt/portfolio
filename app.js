5bbcbb6fd73c27860d10bf6919ea6c00a13f9eaf
Gautier <gautier.fournet@gmail.com>
2023-11-21 11:50:54 +0100

    Changement

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 // Dark - Light Mode
 
 const icon = document.getElementById('icon')
@@ -12,14 +14,13 @@ icon.onclick = function() {
     }
 }
 
-// GSAP
+/* ---------------------------------- GSAP ---------------------------------- */
 
 gsap.to("#bonhomme", {
     scrollTrigger: {
         trigger: "#bonhomme",
-        start: "20px 100%",
+        start: "30px 100%",
         scrub: true,
-        // markers: true
     },
     duration: 1,
     rotation: 360,
@@ -31,14 +32,185 @@ gsap.to("#etoile", {
         trigger: "#etoile",
         start: "20px 100%",
         scrub: true,
-        // markers: true
     },
     duration: 1,
     rotation: -360,
     ease: "none",
 });
 
-// Email.js
+gsap.to(".contain", {
+    scrollTrigger: {
+        trigger: ".contain",
+        start: "20px 100%",
+        end: "30px 0%",
+        scrub: 0.5, // Valeur entre 0 et 1, 0 signifie pas de scrub
+        // markers: true
+    },
+    x: 1000,
+});
+
+gsap.to(".contain2", {
+    scrollTrigger: {
+        trigger: ".contain2",
+        start: "20px 100%",
+        end: "30px 0%",
+        scrub: 0.5, // Valeur entre 0 et 1, 0 signifie pas de scrub
+        // markers: true
+    },
+    x: -1000,
+});
+
+
+gsap.from(".gsap", {
+    scrollTrigger: {
+        trigger: ".gsap",
+        start: "30px 80%",
+        end: '20px 50%',
+        // markers: true
+    },
+    duration: 1,
+    opacity: 0,
+    y: 50,
+    stagger : {
+        each: 0.2,
+        from: 'left'
+    }
+});
+
+gsap.from(".gsap2", {
+    scrollTrigger: {
+        trigger: ".gsap2",
+        start: "30px 80%",
+        end: '20px 50%',
+        // markers: true
+    },
+    duration: 1,
+    opacity: 0,
+    y: 50,
+    stagger : {
+        each: 0.2,
+        from: 'left'
+    }
+});
+
+gsap.from(".gsap3", {
+    scrollTrigger: {
+        trigger: ".gsap3",
+        start: "30px 80%",
+        end: '20px 50%',
+    },
+    duration: 1,
+    opacity: 0,
+    y: 50,
+    stagger : {
+        each: 0.2,
+        from: 'left'
+    }
+});
+
+gsap.from(".gsap4", {
+    scrollTrigger: {
+        trigger: ".gsap4",
+        start: "30px 80%",
+        end: '20px 50%',
+    },
+    duration: 1,
+    opacity: 0,
+    y: 50,
+});
+
+gsap.from(".img", {
+    scrollTrigger: {
+        trigger: ".img",
+        start: "30px 90%",
+        end: '20px 50%',
+    },
+    duration: .5,
+    y: 50,
+    ease: "power1.out"
+});
+
+gsap.from(".span", {
+    duration: 2,
+    opacity: 0,
+    stagger : {
+        each: 0.05,
+        from : "center", //L'animation part du centre
+    },
+});
+
+/* -------------------------------- Barba.js -------------------------------- */
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+function pageTransition() {
+    var tl = gsap.timeline();
+    tl.to(".transition", {
+        duration: 0.8,
+        scaleY: 1,
+        transformOrigin: "bottom",
+        ease: "power4.inOut",
+    });
+
+    tl.to(".transition", {
+        duration: 1,
+        scaleY: 0,
+        transformOrigin: "top",
+        ease: "power4.inOut",
+        delay: 0.2,
+    });
+}
+
+function contentAnimation() {
+    let tl = gsap.timeline();
+
+    tl.to('h1', {
+        top: 0,
+        duration: 1,
+        ease: "power3.inOut",
+        delay: 0.75,
+    })
+
+    
+}
+
+
+barba.init({
+    sync: true,
+    transitions: [
+        {
+            async leave(data) {
+                const done = this.async();
+                
+                pageTransition();
+                await delay(1000);
+                done();
+            },
+
+            async enter(data) {
+                contentAnimation();
+            },
+
+            async once(data) {
+                contentAnimation();
+            }
+        }
+    ]
+})
+
+
+
+
+
+
+/* -------------------------------- Email.js -------------------------------- */
 
 const btn = document.getElementById('submit');
 
@@ -59,3 +231,21 @@ document.getElementById('form').addEventListener('submit', function(event) {
       alert(JSON.stringify(err));
     });
 });
+
+emailjs.init("TL560xq4WlMFu91I-");
+
+
+/* ---------------------------------- Lenis --------------------------------- */
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
